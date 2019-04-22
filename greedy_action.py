8bfb34b7c4f1b4a2fbae3d10bd4cb1fa3a511c60
WongJohnson <WongJohnson@users.noreply.github.com>
2019-04-21 22:49:09 -0400

    cover more cases and add support for local region

--- greedy_action.py
+++ greedy_action.py
@@ -1,23 +1,56 @@
-def greedy_action(grid):
+import numpy as np
+from CrimeWorld import middleMatrix
+
+def greedy_action(grid,radius=-1):
+# given grid of rewards, return action 0-4 moving towards highest reward
+# radius reduces the grid to focus on center only    
+    
+    if radius > -1:
+        grid = middleMatrix(grid,radius)
+    
     ind = np.argmax(grid)
     y,x = np.unravel_index(ind, grid.shape)
-    c = int(grid.shape[0]/2)
-    dx = x-c
-    dy = y-c
+    
+    # center point
+    cx = int(grid.shape[1]/2)
+    cy = int(grid.shape[0]/2)
+    dx = x-cx
+    dy = y-cy
+    
+    # already at max, do nothing
     if dx ==0 and dy==0:
         return 4
-    nx = c-1
-    res_x = 3
-    if dx>0:
-        nx = c+1
-        res_x = 1
     
-    ny = c-1
-    res_y = 2
-    if dy>0:
-        ny = c+1
-        res_y = 0
-    if grid[nx, c] > grid[c, ny]:
-        return res_x
+    if dx > 0:
+        # consider right
+        nx = cx+1
+        res_x = 1
+    elif dx < 0:
+        # consider left
+        nx = cx-1
+        res_x = 3
     else:
+        # consider neither
+        nx = -1
+        
+    if dy > 0:
+        # consider down
+        ny = cy+1
+        res_y = 2
+    elif dy < 0:
+        # consider up
+        ny = cy-1
+        res_y = 0
+    else:
+        # only one choice (move left or right)
+        return res_x
+    
+    # also only one choice (up or down)
+    if nx == -1:
         return res_y
+    
+    # otherwise select which neighbor is best
+    if grid[ny,cx] > grid[cy,nx]:
+        return res_y
+    else:
+        return res_x
