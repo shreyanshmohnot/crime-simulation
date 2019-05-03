cd0f4a72ccaca8d1e10b34cadd54026ebe4a94aa
sbadirli <s.badirli@gmail.com>
2019-05-03 17:40:43 -0400

    converting images into video

--- img2video.py
+++ img2video.py
@@ -1,19 +1,18 @@
-# Converting jpg images into video
-# Need open CV to be installed
 import cv2
-import numpy as np
-import glob
- 
-img_array = []
-for filename in glob.glob('C:/Dektop/Images/*.jpg'):
-    img = cv2.imread(filename)
-    height, width, layers = img.shape
-    size = (width,height)
-    img_array.append(img)
- 
- 
-out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
- 
-for i in range(len(img_array)):
-    out.write(img_array[i])
-out.release()
+import os
+
+image_folder = 'images'
+video_name = 'B_n_TC_evo.avi'
+
+images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
+frame = cv2.imread(os.path.join(image_folder, images[0]))
+height, width, layers = frame.shape
+
+#video = cv2.VideoWriter(video_name, 0, 2, (width,height))
+video = cv2.VideoWriter(video_name, 0, 2, (width,height))
+cv2.VideoWriter_fourcc('M','J','P','G')
+for image in images:
+    video.write(cv2.imread(os.path.join(image_folder, image)))
+
+cv2.destroyAllWindows()
+video.release()
